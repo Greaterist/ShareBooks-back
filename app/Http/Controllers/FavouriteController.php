362fcd950c122822a97fe2b6abba16a3c1abe45b
Ivan <taktaef@yandex.ru>
2023-06-28 23:39:55 +0300

    recommendations API

--- app/Http/Controllers/FavouriteController.php
+++ app/Http/Controllers/FavouriteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Favourite;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class FavouriteController extends Controller
@@ -30,6 +31,7 @@ class FavouriteController extends Controller
      */
     public function store(Request $request)
     {
+
         $validatedData = $request->validate([
             'user_id' => 'required|exists:users,id',
             'book_id' => 'required|exists:books,id',
@@ -87,5 +89,40 @@ class FavouriteController extends Controller
         $books = Favourite::where('user_id', $userId)->with('book')->get();
 
         return response()->json(['books' => $books]);
+    }
+
+    public function getRecommendationsForId(Request $request)
+    {
+
+        $currentUserId = $request->id;
+
+        $current_user = DB::table('favourites')
+            ->where('favourites.user_id', $currentUserId);
+
+        $top_users = DB::table('favourites')
+            ->joinSub($current_user, 'current_user', function ($join) {
+                $join->on('favourites.book_id', '=', 'current_user.book_id');
+            })
+            ->selectRaw('favourites.user_id, COUNT(favourites.book_id) as fav_count')
+            ->where('favourites.user_id', '!=', $currentUserId)
+            ->groupBy('favourites.user_id')
+            ->orderByRaw('fav_count')
+            ->limit(10);
+
+        $result = DB::table('favourites')
+            ->joinSub($top_users, 'top_users', function ($join) {
+                $join->on('favourites.user_id', '=', 'top_users.user_id');
+            })
+            ->join('books', 'favourites.book_id', '=', 'books.id')
+            ->whereNotIn('favourites.book_id', function ($query)  use ($currentUserId) {
+                $query->select('book_id')->from('favourites')->where('user_id', $currentUserId);
+            }) 
+            ->select('books.id', 'books.name', 'books.author', 'books.img', 'books.description')
+            ->groupBy('top_users.fav_count', 'books.id', 'books.name', 'books.author', 'books.img', 'books.description')
+            ->orderByDesc('top_users.fav_count')
+            ->get()
+            ->unique();
+        
+        return response()->json($result);
     }
 }
