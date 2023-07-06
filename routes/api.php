c38f71956a023c8e6cf3a304e38bcb708d6822ed
Александр <sashackovsh@gmail.com>
2023-07-07 00:33:06 +0500

    fixes for auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\FavouriteController;
+use App\Http\Controllers\XMLParseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -23,16 +24,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => ['web']], function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
 Route::get('sanctum/csrf-cookie', function() {
     return response()->json([
         'csrfToken' => csrf_token(),
     ]);
 });
 
-Route::post('/register', [RegisterController::class, 'register']);
-Route::get('/logout', [LoginController::class, 'logout']);
-Route::post('/login', [LoginController::class, 'login']);
-
 
 Route::apiResource('/books', BookController::class);
 Route::apiResource('/favourites', FavouriteController::class);
+
